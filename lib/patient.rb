61f30d6fcb8e6c5deaf3dd1369c6029043e78f32
Yani Asari <yani.iman1@gmail.com>
2020-08-26 21:14:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
   
   def new_appointment(date, doctor)
-    appointment = Appointment.new(date, doctor, self)
+    appointment = Appointment.new(date, self, doctor)
     appointment
   end
   
